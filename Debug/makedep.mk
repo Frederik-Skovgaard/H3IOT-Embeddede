01e4965c9fe045a390f91cd51f6faa3e77485abb
fred56b8 <fred56b8@elevcampus.dk>
2022-06-02 09:09:38 +0200

    message

--- Debug/makedep.mk
+++ Debug/makedep.mk
@@ -2,5 +2,27 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-Main.cpp
+Servo.cpp
+
+src\libraries\Servo\src\avr\Servo.cpp
+
+src\libraries\Servo\src\mbed\Servo.cpp
+
+src\libraries\Servo\src\megaavr\Servo.cpp
+
+src\libraries\Servo\src\nrf52\Servo.cpp
+
+src\libraries\Servo\src\samd\Servo.cpp
+
+src\libraries\Servo\src\sam\Servo.cpp
+
+src\libraries\Servo\src\stm32f4\Servo.cpp
+
+src\libraries\Keypad\src\Key.cpp
+
+src\libraries\Keypad\src\Keypad.cpp
+
+src\libraries\LiquidCrystal\src\LiquidCrystal.cpp
+
+Function\LeapYearFunc.cpp
 
