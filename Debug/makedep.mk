350ed54c004793ea22621adcad8a446eaa76d1b6
fred56b8 <fred56b8@elevcampus.dk>
2022-06-08 13:10:28 +0200

    message

--- Debug/makedep.mk
+++ Debug/makedep.mk
@@ -2,7 +2,51 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-23.1.cpp
+DHT11.cpp
+
+src\libraries\Adafruit_GFX_Library\GFXcanvas\GFXcanvasSerialDemo.cpp
+
+src\libraries\Adafruit_Unified_Sensor\Adafruit_Sensor.cpp
+
+src\libraries\DHT_sensor_library\DHT.cpp
+
+src\libraries\DHT_sensor_library\DHT_U.cpp
+
+src\libraries\DS1307RTC\DS1307RTC.cpp
+
+src\libraries\DS3231\DS3231.cpp
+
+src\libraries\Time\DateStrings.cpp
+
+src\libraries\Time\Time.cpp
+
+src\libraries\Adafruit_BusIO\Adafruit_BusIO_Register.cpp
+
+src\libraries\Adafruit_BusIO\Adafruit_I2CDevice.cpp
+
+src\libraries\Adafruit_BusIO\Adafruit_SPIDevice.cpp
+
+src\libraries\Adafruit_GFX_Library\Adafruit_GFX.cpp
+
+src\libraries\Adafruit_GFX_Library\Adafruit_GrayOLED.cpp
+
+src\libraries\Adafruit_GFX_Library\Adafruit_SPITFT.cpp
+
+src\libraries\Adafruit_GFX_Library\fontconvert\fontconvert.c
+
+src\libraries\Adafruit_GFX_Library\glcdfont.c
+
+src\libraries\Adafruit_SSD1306\Adafruit_SSD1306.cpp
+
+src\libraries\Wire\src\utility\twi.c
+
+src\libraries\Wire\src\Wire.cpp
+
+src\libraries\MFRC522\src\MFRC522.cpp
+
+src\libraries\MFRC522\src\MFRC522Extended.cpp
+
+src\libraries\SPI\src\SPI.cpp
 
 src\libraries\Servo\src\avr\Servo.cpp
 
--- Echo.cpp
+++ Echo.cpp
@@ -1,6 +1,30 @@
-/*
- * Echo.cpp
- *
- * Created: 02/06/2022 12.24.29
- *  Author: frede
- */ 
+/*1. EchoString.cpp */
+
+// Demo af Serial.available() Serial.print() og Serial.println(). Benyt Terminal Window uden LF og CR
+
+#include <Arduino.h>
+
+int incomingByte = 0; // for incoming serial data
+
+void setup()
+{
+	Serial.begin(115200);		// opens serial port, sets data rate to 9600 bps
+	while(!Serial);				// used on SAMD boards
+	Serial.println("Serial 9600 baud!");
+}
+
+void loop()
+{
+	// reply only when you receive data:
+	if (Serial.available() > 0)
+	{
+		// read the incoming byte from Buffer:
+		incomingByte = Serial.read();
+
+		// say what you got:
+		Serial.print("I received: ");
+		Serial.print((char)incomingByte);
+		Serial.print(" = ");
+		Serial.println(incomingByte, HEX);
+	}
+}
